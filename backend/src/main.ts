281a152b12a21aa1c32182fc362b3ab20a32803c
jinsy731 <jinsy731@gmail.com>
2024-03-01 22:32:31 +0900

    Configure Swagger Module

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,34 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+  .setTitle("Mini Drive API")
+  .setDescription("Mini Drive API")
+  .setVersion("1.0")
+  .addBearerAuth(
+    {
+      // I was also testing it without prefix 'Bearer ' before the JWT
+      description: `[just text field] Please enter token in following format: Bearer <JWT>`,
+      name: "Authorization",
+      bearerFormat: "Bearer", // I`ve tested not to use this field, but the result was the same
+      scheme: "Bearer",
+      type: "http", // I`ve attempted type: 'apiKey' too
+      in: "Header",
+    },
+    "access-token", 
+  )
+  .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup("api", app, document)
+
+  await app.listen(8080);
 
   if (module.hot) {
     module.hot.accept();
